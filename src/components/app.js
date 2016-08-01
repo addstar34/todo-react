2c4c2f93ebaaae36b57b08e9c632f539cae1e1fc
Adam D <addstar34@gmail.com>
2016-08-01 17:21:12 +1000

    update webpack

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,16 @@ import TodosList from './todos-list';
 
 const todos = [
   {
-    task: 'make React tutorial',
+    task: 'eat',
     isCompleted: false
   },
   {
-    task: 'eat dinner',
+    task: 'coding',
     isCompleted: true
+  },
+  {
+    task: 'sleep',
+    isCompleted: false
   }
 ];
 
